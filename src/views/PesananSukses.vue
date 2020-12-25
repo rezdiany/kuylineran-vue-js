97a4913a3a7f78a657ab7ec7d65e1d0e100665f7
rezdiany <63382198+rezdiany@users.noreply.github.com>
2020-12-25 19:20:38 +0700

    Add files via upload

---
+++ src/views/PesananSukses.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pesanan-sukses">
+    <Navbar />
+    <div class="container">
+      <div class="row justify-content-center mt-5">
+        <div class="col text-center">
+          <img src="../assets/images/menunggu.png" width="300" />
+          <h2><String>Sukses Memesan!</String></h2>
+          <h4>
+            Pesanan Anda segera diproses<br />
+            Silahkan Menunggu
+          </h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  name: "PesananSukses",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+</style>
